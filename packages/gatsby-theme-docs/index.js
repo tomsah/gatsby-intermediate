3ce4cacd89037523cfdb6d7609c99f3604335876
Thomas Salah <thomas.salah@gmail.com>
2020-07-25 11:50:12 +0100

    feat(theme-dev) workspace setup

---
+++ packages/gatsby-theme-docs/index.js
@@ -0,0 +1,5 @@
+// NECESSARY EMPTY FILE
+// in the package.json we have a main property pointing to index.js
+// when you try to use a package the first things node does, is to look
+// at the main and make sure that whatever the file is pointing to exists
+//if it doesn't, it will say the package is invalid
